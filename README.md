65a8d196e698ca8810b0a49460c58a530eaba4d4
maticjesenicnik <matic.jesenicnik@gmail.com>
2022-08-03 14:11:02 +0200

    feat: accordion animation

---
+++ README.md
@@ -0,0 +1,6 @@
+# Interactive Accordion
+
+Interactive accordion learning and testing software made with HTML and CSS and using jquery. Project will allow people to create their own songs, save them, and share them with friends. 
+
+**Note:** Project thrown together quickly and doesn't really have in mind responsive design at this time. 
+
--- main.js
+++ main.js
@@ -6,4 +6,9 @@ $(() => {
   $('.bass-btn').click((e) => {
     console.log(`bass number: ${$(e.target).data("value-num")}`);
   })
+
+  $('.btn-animation-toggle').click((e) => {
+    $('#flex-container').toggleClass('animation-paused');
+    $('.btn-animation-toggle').text($('#flex-container').hasClass('animation-paused') ? 'Play' : 'Pause');
+  });
 })
